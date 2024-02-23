0d2f0a0b93a08a60e770b27b4e04ae43d2f09eb8
Crl-Vlz <crlvlz0215@gmail.com>
2024-02-23 11:21:57 -0600

    Added some KPIs

--- ProductionLine.py
+++ ProductionLine.py
@@ -17,7 +17,7 @@ class Item:
             self.stages_passed.append(workstation_id)
 
 class WorkStation:
-    def __init__(self, _env: simpy.Environment, _resource: simpy.Resource, id : int, fail_rate : float, next_stations : list) -> WorkStation :
+    def __init__(self, _env: simpy.Environment, _resource: simpy.Resource, id : int, fail_rate : float, next_stations : list, _name : str) -> WorkStation :
         self.env : simpy.Environment = _env
         self.resource = resource
         self.action = self.env.process(self.run())
@@ -26,6 +26,12 @@ class WorkStation:
         self.item = None
         self.fail_rate : float = fail_rate
         self.id = id
+        
+        #KPIs
+        self.wait_time = 0.0
+        self.work_time = 0.0
+        self.repair_time = 0.0
+        self.total_time = 0.0
         
     def process_item(self):
         if self.id == 1:
@@ -44,19 +50,29 @@ class WorkStation:
             print("No item")
         
     def run(self) -> None: # type: ignore
+        #Init some KPIs for output
+        start_time = self.env.now
         while True:
             if self.id == 1:
                 self.item = Item()
-            if random.random() <= self.fail_rate:
-                print(f"Station {self.id} Failure")
-                print(f"Station {self.id} started repairs at {self.env.now:.2f}")
-                yield self.env.timeout(random.normalvariate(3))
-                print(f"Station {self.id} finished repairs at {self.env.now:.2f}") 
-            if self.item and self.raw_materials:
-                yield self.env.timeout(random.normalvariate(4))
-                self.item.process(self.id)
-                self.raw_materials = self.raw_materials - 1
-                print("Made item")
+            if self.item:
+                if random.random() <= self.fail_rate:
+                    print(f"Station {self.id} Failure")
+                    print(f"Station {self.id} started repairs at {self.env.now:.2f}")
+                    yield self.env.timeout(random.normalvariate(3))
+                    print(f"Station {self.id} finished repairs at {self.env.now:.2f}")
+                if self.raw_materials:
+                    self.wait_time = self.env.now - start_time + self.wait_time
+                    start_time = self.env.now
+                    yield self.env.timeout(random.normalvariate(4))
+                    self.item.process(self.id)
+                    self.raw_materials = self.raw_materials - 1
+                    print("Made item")
+                    self.work_time = self.work_time + (self.env.noow - start_time)
+                    #TODO send item to next workstation
+                    self.item = None
+                    start_time = self.env.now
+                #TODO add an else clause to wait for raw materials
             else:
                 print("No item")
             
@@ -64,5 +80,5 @@ class WorkStation:
 env = simpy.Environment()
 resource = simpy.Resource(env, 3)
 
-station1 = WorkStation(env, resource, 1, 0.20, [2])
+station1 = WorkStation(env, resource, 1, 0.20, [2], "assembly")
 env.run(until=200)
