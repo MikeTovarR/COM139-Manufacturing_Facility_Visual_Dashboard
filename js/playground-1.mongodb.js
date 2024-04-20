6357556a1dde753f8d07876e107371fd0989fa39
LOLImaze <andresmtzc10@hotmail.com>
2024-04-19 20:13:02 -0600

    Working json

--- js/playground-1.mongodb.js
+++ js/playground-1.mongodb.js
@@ -13,16 +13,70 @@
 use('ProductionLine');
 
 // Insert a few documents into the sales collection.
-db.getCollection('Runs').insertMany([
-  { 'item': 'abc', 'price': 10, 'quantity': 2, 'date': new Date('2014-03-01T08:00:00Z') },
-  { 'item': 'jkl', 'price': 20, 'quantity': 1, 'date': new Date('2014-03-01T09:00:00Z') },
-  { 'item': 'xyz', 'price': 5, 'quantity': 10, 'date': new Date('2014-03-15T09:00:00Z') },
-  { 'item': 'xyz', 'price': 5, 'quantity': 20, 'date': new Date('2014-04-04T11:21:39.736Z') },
-  { 'item': 'abc', 'price': 10, 'quantity': 10, 'date': new Date('2014-04-04T21:23:13.331Z') },
-  { 'item': 'def', 'price': 7.5, 'quantity': 5, 'date': new Date('2015-06-04T05:08:13Z') },
-  { 'item': 'def', 'price': 7.5, 'quantity': 10, 'date': new Date('2015-09-10T08:43:00Z') },
-  { 'item': 'abc', 'price': 10, 'quantity': 5, 'date': new Date('2016-02-06T20:20:13Z') },
-]);
+db.getCollection('Runs').insert_one({
+  "2024-04-19-07-45": [
+      {
+          "STATION": 1,
+          "PERIOD": "Month",
+          "PRODUCTION": 108,
+          "OCCUPANCY": 423.114605,
+          "DOWNTIME": 60.617189,
+          "FIX_TIME": 6.605775,
+          "WAITING_TIME": 9.1,
+          "BOTTLENECK_TIME": 51.517189
+      },
+      {
+          "STATION": 2,
+          "PERIOD": "Month",
+          "PRODUCTION": 108,
+          "OCCUPANCY": 423.114605,
+          "DOWNTIME": 60.617189,
+          "FIX_TIME": 6.605775,
+          "WAITING_TIME": 9.1,
+          "BOTTLENECK_TIME": 51.517189
+      },
+      {
+          "STATION": 3,
+          "PERIOD": "Month",
+          "PRODUCTION": 108,
+          "OCCUPANCY": 423.114605,
+          "DOWNTIME": 60.617189,
+          "FIX_TIME": 6.605775,
+          "WAITING_TIME": 9.1,
+          "BOTTLENECK_TIME": 51.517189
+      },
+      {
+          "STATION": 4,
+          "PERIOD": "Month",
+          "PRODUCTION": 108,
+          "OCCUPANCY": 423.114605,
+          "DOWNTIME": 60.617189,
+          "FIX_TIME": 6.605775,
+          "WAITING_TIME": 9.1,
+          "BOTTLENECK_TIME": 51.517189
+      },
+      {
+          "STATION": 5,
+          "PERIOD": "Month",
+          "PRODUCTION": 108,
+          "OCCUPANCY": 423.114605,
+          "DOWNTIME": 60.617189,
+          "FIX_TIME": 6.605775,
+          "WAITING_TIME": 9.1,
+          "BOTTLENECK_TIME": 51.517189
+      },
+      {
+          "STATION": 6,
+          "PERIOD": "Month",
+          "PRODUCTION": 108,
+          "OCCUPANCY": 423.114605,
+          "DOWNTIME": 60.617189,
+          "FIX_TIME": 6.605775,
+          "WAITING_TIME": 9.1,
+          "BOTTLENECK_TIME": 51.517189
+      }
+  ]
+});
 /*
 // Run a find command to view items sold on April 4th, 2014.
 const salesOnApril4th = db.getCollection('sales').find({
